d976132bd8555753bac52de7415da06f78af3681
Diego Freile Garcia <dfreile51@gmail.com>
2023-11-09 18:51:17 +0100

    Funcionalidad de crear tags creada y con estilos

---
+++ src/js/tags.js
@@ -0,0 +1,63 @@
+(function () {
+    const tagsInput = document.querySelector("#tags_input");
+
+    // Si existe el input...
+    if (tagsInput) {
+        const tagsDiv = document.querySelector("#tags");
+        const tagsInputHidden = document.querySelector("[name='tags']");
+
+        let tags = [];
+
+        // Escuchar los cambios en el input
+        tagsInput.addEventListener("keypress", guardarTag);
+
+        function guardarTag(e) {
+            // Si se presiono la coma...
+            if (e.keyCode === 44) {
+                e.preventDefault();
+
+                if (e.target.value.trim() === "" || e.target.value.length < 1) {
+                    return;
+                }
+
+                tags = [...tags, e.target.value.trim()];
+
+                tagsInput.value = "";
+
+                mostrarTags();
+            }
+        }
+
+        function mostrarTags() {
+            limpiarTags();
+
+            tags.forEach(tag => {
+                const etiqueta = document.createElement("LI");
+                etiqueta.classList.add("formulario__tag");
+                etiqueta.textContent = tag;
+                etiqueta.ondblclick = eliminarTag;
+                tagsDiv.appendChild(etiqueta);
+            });
+
+            actualizarInputHidden();
+        }
+
+        function actualizarInputHidden() {
+            tagsInputHidden.value = tags.toString();
+        }
+
+        function eliminarTag(e) {
+            e.target.remove();
+
+            tags = tags.filter(tag => tag !== e.target.textContent);
+
+            actualizarInputHidden();
+        }
+
+        function limpiarTags() {
+            while (tagsDiv.firstChild) {
+                tagsDiv.removeChild(tagsDiv.firstChild);
+            }
+        }
+    }
+})();
